28c923201b434a5254cb13bdae00720af7f77f1f
unknown <263833322@qq.com>
2018-04-24 11:01:05 +0800

    modify login server

--- login_servers.sh
+++ login_servers.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-ssh chinatsp@10.100.129.111
+VALUE=111
+if [ ! -n "$1" ];
+then
+	VALUE=111
+else
+	VALUE=$1
+fi
+
+case ${VALUE} in
+	82)
+		ssh tangqishun@10.100.129.82
+		;;
+	*)
+		ssh chinatsp@10.100.129.111
+		;;
+esac
+
+
+
+
 
